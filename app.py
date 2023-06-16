fbeda5ec45e6c5b96693283e88dae6d3d1c36397
uzenn <andrewnagatha@gmail.com>
2023-06-16 15:02:38 +0700

    deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from main import model
+import os
 
 app = Flask(__name__)
 
@@ -19,5 +20,9 @@ def solve():
 
     return jsonify(response)
 
+@app.route('/', methods=['GET'])
+def index():
+    return 'ML API'
+
 if __name__ == '__main__':
-       app.run(host='0.0.0.0', port=5000, debug=False)
+      app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
